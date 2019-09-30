980f416710b3db539916ed81a0b9e5fe68339c7b
Pedro <phamorimrezende@gmail.com>
2019-09-29 23:57:55 -0300

    Remover_cenarios

--- ns-3.29/build/build-status.py
+++ ns-3.29/build/build-status.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python
 
 # Programs that are runnable.
-ns3_runnable_programs = ['build/scratch/ns3.29-scratch-simulator-debug', 'build/scratch/ns3.29-lte_backup3-debug', 'build/scratch/ns3.29-lte_backup2-debug', 'build/scratch/ns3.29-futsal-simple-scenario-debug', 'build/scratch/ns3.29-lte_backup-debug', 'build/scratch/ns3.29-exemplo_lte-debug', 'build/scratch/ns3.29-exemplo-debug', 'build/scratch/subdir/ns3.29-subdir-debug']
+ns3_runnable_programs = ['build/scratch/ns3.29-scratch-simulator-debug', 'build/scratch/ns3.29-futsal-simple-scenario-debug', 'build/scratch/subdir/ns3.29-subdir-debug']
 
 # Scripts that are runnable.
 ns3_runnable_scripts = []
